bcd8ea4f69c483ab1d0e6a5ba7df35a92e128b41
AkshitaShinkar <111171852+AkshitaShinkar@users.noreply.github.com>
2022-08-14 11:44:30 +0530

    Add files via upload

---
+++ Content/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.DashboardWeb;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace DashboardMvcDemo.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            if (Request.Browser.IsMobileDevice) {
+                return View("MobileDashboard");
+            }
+            string workingModeString = this.Request.QueryString["mode"];
+            var workingMode = !string.IsNullOrEmpty(workingModeString) && workingModeString == "designer" ? WorkingMode.Designer : WorkingMode.Viewer;
+            
+            return View(workingMode);
+        }
+        [Route("MobileDashboard")]
+        public ActionResult MobileDashboard() {
+            return View();
+        }
+        [Route("Emulator")]
+        public ActionResult Emulator() {      
+            return View("Emulator", null, ConfigurationManager.AppSettings["MobileUrl"]);            
+        }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web.Routing;
+using System.Xml.Linq;
+using DevExpress.Security.Resources;
+
+namespace DevExpress.Web.Demos {
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication {
+        
+
+        protected void Application_Start() {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            DashboardConfig.RegisterService(RouteTable.Routes);
+            AccessSettings.StaticResources.TrySetRules(DirectoryAccessRule.Allow(Server.MapPath(@"~/Content")), UrlAccessRule.Allow());
+            DevExpress.Web.ASPxWebControl.CallbackError += new EventHandler(CallbackError);
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls | System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls12;
+        }
+
+
+        protected void Session_Start() {
+            RegisterDefaultDashboard("SalesOverview");
+            RegisterDefaultDashboard("CustomerSupport");
+            RegisterDefaultDashboard("SalesPerformance");
+            RegisterDefaultDashboard("SalesDetails");
+            RegisterDefaultDashboard("Financial");
+            RegisterDefaultDashboard("EnergyStatistics");
+            RegisterDefaultDashboard("HumanResources");
+            RegisterDefaultDashboard("ChampionsLeagueStatistics");
+            RegisterDefaultDashboard("RevenueAnalysis");
+            RegisterDefaultDashboard("RevenueByIndustry");
+            RegisterDefaultDashboard("EnergyConsumption");
+            RegisterDefaultDashboard("WebsiteStatistics");
+            RegisterDefaultDashboard("EUTradeOverview");
+            RegisterDefaultDashboard("YTDPerformance");
+            RegisterDefaultDashboard("DateFilter");
+            RegisterDefaultDashboard("DataFederation");
+            RegisterDefaultDashboard("ProductDetails");
+            RegisterDefaultDashboard("CustomItemExtensions");
+        }
+        void RegisterDefaultDashboard(string dashboardId) {
+            string dashboardLocalPath = Server.MapPath(string.Format(@"~/App_Data/Dashboards/{0}.xml", dashboardId));
+            SessionDashboardStorage.Instance.RegisterDashboard(dashboardId, XDocument.Load(dashboardLocalPath));
+        }
+
+        void CallbackError(object sender, EventArgs e) {
+            // Logging exceptions occur on callback events of DevExpress ASP.NET MVC controls. 
+            // To learn more, see http://www.devexpress.com/Support/Center/Example/Details/E4588
+        }
+    }
+}
